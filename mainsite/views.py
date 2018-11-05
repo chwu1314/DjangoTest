cad976b9aff2905eecb2f039f678f966edecc6e2
chwu1314 <mado40035@gmail.com>
2018-11-05 16:27:14 +0800

    template

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Post
+from datetime import datetime
+from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def homepage(request):
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
-        post_lists.append("<hr><small>" + str(post.body) + "</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
--- mblog/urls.py
+++ mblog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
-from mainsite.views import homepage
+from mainsite.views import homepage, showpost
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path('post/<slug:slug>/', showpost),
 ]
